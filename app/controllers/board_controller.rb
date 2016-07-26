6857ac6216eebf99dadc9e929959ca027fa36f4c
sungwu <swcho11@naver.com>
2016-07-26 10:43:18 +0000

    내용을입력해주세요

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -4,13 +4,18 @@ class BoardController < ApplicationController
     else
       redirect_to '/users/sign_in'
     end
-    
+
     @post = Post.all
   end
   def create
-    @post = Post.new(title: params[:title], content: params[:content])
-    @post.save
-    redirect_to :back
+    @post = Post.new(content: params[:content])
+    if @post.save
+      redirect_to "/"
+    else
+      flash[:notice] = "내용을 입력해주세요."
+      redirect_to "/"
+    end
+
   end
 
   def new
@@ -21,7 +26,7 @@ class BoardController < ApplicationController
 
   def destroy
   end
-  
+
   def comment_create
     @comment = Comment.new(post_id: params[:post_id], content: params[:content])
     @comment.save
@@ -29,9 +34,9 @@ class BoardController < ApplicationController
   end
   def search
   end
-  
+
   def search_view
     @search_post = Post.search(params[:content])
-    
+
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    validates :content, presence: { message: "제목을 작성해주세요."}
     include SearchCop
 
     search_scope :search do
