eaa04ec1aa86ca4d6a13ab163fcc195dbbf32cae
unknown <>
2016-08-18 21:09:11 +0900

    aws id delete

--- config/initializers/awsSungwu.rb
+++ config/initializers/awsSungwu.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|                     # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIPU3WOXPPYADOYQQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     host:                  's3.example.com',             # optional, defaults to nil
     endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
